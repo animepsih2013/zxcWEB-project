416b9ee3bacecc16a81ff9e55d94a71aab4eda5f
PabloEskabarro <nonzxckid76@gmail.com>
2025-04-03 23:49:08 +0300

    update 03.04

---
+++ main.py
@@ -0,0 +1,19 @@
+import subprocess
+import sys
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+# Чтение файла requirements.txt и установка библиотек
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+for package in required_packages:
+    try:
+        __import__(package)
+    except ImportError:
+        print(f"Установка {package}...")
+        install(package)
+

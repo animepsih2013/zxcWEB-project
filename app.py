825dc85badcfe9df4f194d59e477771d681685b4
KiAMurrA <sergey.uin.7@gmail.com>
2025-05-06 23:22:00 +0300

    start_1.0

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8888, host='127.0.0.1', debug=True)

811c0138b7a410e856b313f8559bdf2a3d78cf35
KiAMurrA <sergey.uin.7@gmail.com>
2025-05-08 01:46:24 +0300

    start_2.0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,9 +9,19 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
